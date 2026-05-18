4c0d097567118212ab959f32f0e5f95f454c4257
Carbec <carbec.lorient@net-c.fr>
2026-05-18 17:57:10 +0200

    Enable USE_TUNING for SPSA branch
    marges
    
    total nodes = 27621053

--- src/Tunable.h
+++ src/Tunable.h
@@ -88,8 +88,8 @@ PARAM(RazoringDepth,    3, 1, 6);
 PARAM(RazoringMargin, 200, 100, 300);
 
 //  STATIC NULL MOVE PRUNING ou aussi REVERSE FUTILITY PRUNING
-PARAM(SNMPDepth,                6,   2,  10);
-PARAM(SNMPMargin,              70,  50, 100);
+PARAM(SNMPDepth,                6,   2,  12);
+PARAM(SNMPMargin,              70,  50, 120);
 PARAM(SNMPCorrplexityScale,    60,   0, 128);
 
 // Correction History (constantes de Halogen)
