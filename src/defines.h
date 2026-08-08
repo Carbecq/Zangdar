4d271ef925376631143864ba902c58cba4f0978a
Carbec <carbec.lorient@net-c.fr>
2026-08-08 08:43:45 +0200

    7.23
    + HASH_SIZE = 16 Mo
    	> le bench mesure maintenant la qualité de la TT (remplacement, âge, indexation).
    	  Un patch sur la politique de remplacement devient visible au bench .
    
    total nodes = 20913207

--- src/defines.h
+++ src/defines.h
@@ -41,7 +41,7 @@ static constexpr int MAX_HISTO  = 1024;     // longueur max de l'historique (par
 static constexpr int MAX_MOVES  =  256;     // taille max d'une liste de coups. Le maximum théorique pour une position légale est 218
 static constexpr int MAX_TIME   = 60*60*1000;   // 1 heure en ms
 
-static constexpr int HASH_SIZE      = 128;      // en Mo
+static constexpr int HASH_SIZE      = 16;       // en Mo
 static constexpr int MIN_HASH_SIZE  = 1;
 static constexpr int MAX_HASH_SIZE  = 16384;    // 16 Go
 
