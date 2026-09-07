a909e0ffa9a724100777a9a1fe8f4684483066a7
Carbec <carbec.lorient@net-c.fr>
2026-09-07 11:46:14 +0200

    7.30
    + SNMPCorrplexityScale : défaut ramené de 60 à 30
    
    total nodes = 22698883

--- src/Tunable.h
+++ src/Tunable.h
@@ -93,7 +93,7 @@ PARAM(RazoringMargin, 200, 100, 300);
 //  STATIC NULL MOVE PRUNING ou aussi REVERSE FUTILITY PRUNING
 PARAM(SNMPDepth,                6,   2,  10);
 PARAM(SNMPMargin,              70,  50, 100);
-PARAM(SNMPCorrplexityScale,    60,   0, 128);
+PARAM(SNMPCorrplexityScale,    30,   0, 128);
 
 // Correction History (constantes de Halogen)
 PARAM(PawnCorrScale,         109,   64,  256);
